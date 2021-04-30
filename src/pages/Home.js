cf76d31bc8143f383636c95bd4ddd2a4505746cc
Eyimofe Ogunbiyi <ogunbiyioladapo33@gmail.com>
2021-04-30 13:06:17 +0100

    header impl

---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import Header from '../components/header';
+
+const Home = function () {
+    return (
+        <>
+            <Header/>
+            <h1>Home page</h1>    
+        </>
+    )
+}
+
+export default Home;
