fd861efb4839ac5ded848959228956457996b8aa
Eyimofe Ogunbiyi <ogunbiyioladapo33@gmail.com>
2021-05-04 15:04:21 +0100

    completed home page responsiveness impl

---
+++ src/components/Footer.js
@@ -0,0 +1,19 @@
+import '../css/footer.css';
+const Footer = () => {
+    return (
+        <footer className='footer'>
+            <div className='footer-left'>
+                <p>Some Link</p>
+                <p>Some Link</p>
+                <p>Some Link</p>
+            </div>
+            <div className='footer-right'>
+                <p>Some Link</p>
+                <p>Some Link</p>
+                <p>Some Link</p>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer;
